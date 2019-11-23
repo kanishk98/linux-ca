e8375acdbe728472e94abf92ad1e48caffea38bd
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-11-23 17:28:52 +0530

    getAllCerts: Add async reads capability.

--- src/index.js
+++ src/index.js
@@ -2,25 +2,40 @@ const fs = require("fs");
 
 const { paths } = require("../res/linux-cert-stores.json");
 const splitPattern = /(?=-----BEGIN\sCERTIFICATE-----)/g;
+const noCertsError = new Error(
+  "No certificates were found. If you think this is a bug, please report it at https://github.com/kanishk98/linux-ca/issues"
+);
 
 const getAllCerts = (onData, onError, readSync = false) => {
   // TODO: Include https module certs here too
   const certs = [];
-  for (const path of paths) {
-    try {
-      const file = fs.readFileSync(path, "utf-8");
-      certs.push(file.split(splitPattern).map(cert => cert));
-      onData(certs);
-      break;
-    } catch (err) {
-      onError(err);
+  if (readSync) {
+    for (const path of paths) {
+      try {
+        const file = fs.readFileSync(path, "utf-8");
+        certs.push(file.split(splitPattern).map(cert => cert));
+        onData(certs);
+        break;
+      } catch (err) {
+        onError(err);
+      }
+      // control reaches here only when all distro paths failed
+      onError(noCertsError);
     }
-    // control reaches here only when all distro paths failed
-    onError(
-      new Error(
-        "No certificates were found. If you think this is a bug, please report it at https://github.com/kanishk98/linux-ca/issues"
-      )
-    );
+  } else {
+    for (const path of paths) {
+      fs.readFile(path, "utf-8", (err, file) => {
+        if (err) {
+          onError(err);
+        } else {
+          if (!file) {
+            return;
+          }
+          certs.push(file.split(splitPattern).map(cert => cert));
+          onData(certs);
+        }
+      });
+    }
   }
 };
 
