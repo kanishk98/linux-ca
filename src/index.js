5268bfbf5cdc7af8502484e2df189a3ba6852560
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-11-24 02:00:22 +0530

    getAllCerts: Refactor method to return Promise(certs).

--- src/index.js
+++ src/index.js
@@ -8,37 +8,37 @@ const noCertsError = new Error(
   "No certificates were found. If you think this is a bug, please report it at https://github.com/kanishk98/linux-ca/issues"
 );
 
-const getAllCerts = (onData, onError, readSync = false) => {
-  const certs = (https.globalAgent.options.ca =
-    https.globalAgent.options.ca || []);
-  if (readSync) {
-    for (const path of paths) {
-      try {
-        const file = fs.readFileSync(path, "utf-8");
-        certs.push(file.split(splitPattern).map(cert => cert));
-        onData(certs);
-        break;
-      } catch (err) {
-        onError(err);
-      }
-      // control reaches here only when all distro paths failed
-      onError(noCertsError);
-    }
-  } else {
-    for (const path of paths) {
-      fs.readFile(path, "utf-8", (err, file) => {
-        if (err) {
-          onError(err);
-        } else {
-          if (!file) {
-            return;
-          }
+const getAllCerts = (readSync = false) => {
+  return new Promise((resolve, reject) => {
+    const certs = (https.globalAgent.options.ca =
+      https.globalAgent.options.ca || []);
+    if (readSync) {
+      for (const path of paths)
+        try {
+          const file = fs.readFileSync(path, "utf-8");
           certs.push(file.split(splitPattern).map(cert => cert));
-          onData(certs);
+          resolve(certs);
+        } catch (err) {
+          if (err.code !== "ENOENT") {
+            reject(err);
+          }
         }
-      });
+    } else {
+      for (const path of paths) {
+        fs.readFile(path, "utf-8", (err, file) => {
+          if (err) {
+            if (err.code !== "ENOENT") {
+              reject(err);
+            }
+          } else {
+            certs.push(file.split(splitPattern).map(cert => cert));
+            resolve(certs);
+          }
+        });
+      }
     }
-  }
+    reject(noCertsError);
+  });
 };
 
 const onDataPromise = (data, subject) => {
